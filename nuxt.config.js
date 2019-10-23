b1b523afba3e713b9def6bbad307e0e0a1e2d025
Morten Sørensen <morten@moso.io>
2019-10-23 15:35:47 +0200

    Updated Nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+export default {
+    /*
+    ** SSR + modern mode
+    */
+    mode: 'universal',
+
+    modern: 'client',
+
     /*
     ** Headers of the page
     */
@@ -6,16 +13,16 @@ module.exports = {
         title: 'Bio | moso.io',
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
             { vmid: 'og:description', property: 'og:description', content: 'Bio made with Vue.js and Nuxt.js' },
-            { vmid: 'og:image', property: 'og:image', content: 'https://moso.io/images/og_image.png'},
-            { vmid: 'og:image:secure_url', property: 'og:image:secure_url', content: 'https://moso.io/images/og_image.png'},
-            { vmid: 'og:image:width', property: 'og:image:width', content: '700'},
-            { vmid: 'og:image:height', property: 'og:image:height', content: '500'},
-            { vmid: 'og:title', property: 'og:title', content: 'Bio | moso.io'},
-            { vmid: 'og:type', property: 'og:type', content: 'website'},
-            { vmid: 'og:url', property: 'og:url', content: 'https://bio.moso.io'},
-            { name: 'description', name: 'description', content: 'Bio made with Vue.js and Nuxt.js' },
+            { vmid: 'og:image', property: 'og:image', content: 'https://moso.io/images/og_image.png' },
+            { vmid: 'og:image:secure_url', property: 'og:image:secure_url', content: 'https://moso.io/images/og_image.png' },
+            { vmid: 'og:image:width', property: 'og:image:width', content: '700' },
+            { vmid: 'og:image:height', property: 'og:image:height', content: '500' },
+            { vmid: 'og:title', property: 'og:title', content: 'Bio | moso.io' },
+            { vmid: 'og:type', property: 'og:type', content: 'website' },
+            { vmid: 'og:url', property: 'og:url', content: 'https://bio.moso.io' },
+            { name: 'description', content: 'Bio made with Vue.js and Nuxt.js' },
             { name: 'author', content: 'Morten Sørensen (morten@moso.io)' },
             { name: 'mobile-web-app-capable', content: 'yes' },
             { name: 'apple-mobile-web-app-capable', content: 'yes' },
@@ -34,7 +41,7 @@ module.exports = {
     ** CSS
     */
     css: [
-        '~/node_modules/flexgrid.co/css/flexgrid-tailwind.css',
+        '~/node_modules/flexgrid.io/dist/flexgrid.min.css',
         '~/assets/sass/main.scss'
     ],
 
@@ -47,20 +54,17 @@ module.exports = {
     ** Third-party modules
     */
     modules: [
-        '@nuxtjs/axios',
+        'nuxt-webfontloader'
     ],
 
     /*
-    ** axios config
+    ** Webfontloader
     */
-    axios: {
-        proxy: true
+    webfontloader: {
+        google: {
+            families: ['Source+Sans+Pro:300,400,600&display=swap']
+        }
     },
-
-    proxy: {
-        '/api/': { target: 'http://api.morten.is', pathRewrite: {'^/api/': ''} }
-    },
-
 
     /*
     ** Build configuration
