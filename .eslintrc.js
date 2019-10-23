38e9438291eaad2dca105380bb06776c71fecb8d
Morten Sørensen <morten@moso.io>
2019-10-23 15:44:30 +0200

    Addednew linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {}
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'prettier',
+        'prettier/vue',
+        'plugin:nuxt/recommended',
+        'plugin:vue/essential',
+    ],
+    // required to lint *.vue files
+    plugins: [
+        'prettier',
+        'vue'
+    ],
+    // add your custom rules here
+    rules: {}
+  }
