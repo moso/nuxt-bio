2153d206b2368a015fa60db14e52bc31a09c71ac
Morten Sørensen <morten@moso.io>
2019-11-15 12:00:35 +0100

    Updated linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true
-    },
-    parserOptions: {
-        parser: 'babel-eslint'
-    },
-    extends: [
-        '@nuxtjs',
-        'prettier',
-        'prettier/vue',
-        'plugin:nuxt/recommended',
-        'plugin:vue/essential',
-    ],
-    // required to lint *.vue files
-    plugins: [
-        'prettier',
-        'vue'
-    ],
-    // add your custom rules here
-    rules: {}
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:nuxt/recommended',
+    'plugin:vue/essential',
+  ],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    indent: ["error", 4],
+    "space-before-function-paren": ["error", {
+    "anonymous": "never",
+    "named": "never",
+    "asyncArrow": "always"
+    }]
   }
+}
