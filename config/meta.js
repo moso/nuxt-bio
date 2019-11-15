eb308d7523cc96e498dd3292e730fb4ed1c942d0
Morten Sørensen <morten@moso.io>
2019-11-15 12:05:06 +0100

    Added meta config

---
+++ config/meta.js
@@ -0,0 +1,18 @@
+export default {
+    lang: 'en',
+    mobileAppIOS: true,
+    name: 'Bio | moso.io', // Change me
+    author: 'Morten Sørensen', // Change me
+    description: 'Bio made with Vue.js and Nuxt.js', // Change me
+    ogTitle: 'Bio | moso.io', // Change me
+    ogHost: 'https://bio.moso.io', // Change me
+    ogImage: {
+        path: 'https://moso.io/images/og_image.png', // Change me
+        width: '1200',
+        height: '630',
+        type: 'image/png'
+    },
+    twitterCard: 'summary',
+    twitterCreator: '@mosolicious', // Change me
+    twitterSite: '@mosolicious' // Change me
+}
