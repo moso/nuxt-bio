6b2385a6830d6fc005e0c936c1f151013bb603d5
Morten Sørensen <morten@moso.io>
2019-11-15 12:01:32 +0100

    Nuxt config clean-up

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,88 +1,43 @@
+import { build, dev, env, manifest, meta } from './config'
+
+require('dotenv').config()
+
 export default {
-    /*
-    ** SSR + modern mode
-    */
     mode: 'universal',
 
-    modern: 'client',
+    modern: !dev.isDev && 'client',
 
-    /*
-    ** Headers of the page
-    */
-    head: {
-        title: 'Bio | moso.io',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-            { vmid: 'og:description', property: 'og:description', content: 'Bio made with Vue.js and Nuxt.js' },
-            { vmid: 'og:image', property: 'og:image', content: 'https://moso.io/images/og_image.png' },
-            { vmid: 'og:image:secure_url', property: 'og:image:secure_url', content: 'https://moso.io/images/og_image.png' },
-            { vmid: 'og:image:width', property: 'og:image:width', content: '700' },
-            { vmid: 'og:image:height', property: 'og:image:height', content: '500' },
-            { vmid: 'og:title', property: 'og:title', content: 'Bio | moso.io' },
-            { vmid: 'og:type', property: 'og:type', content: 'website' },
-            { vmid: 'og:url', property: 'og:url', content: 'https://bio.moso.io' },
-            { name: 'description', content: 'Bio made with Vue.js and Nuxt.js' },
-            { name: 'author', content: 'Morten Sørensen (morten@moso.io)' },
-            { name: 'mobile-web-app-capable', content: 'yes' },
-            { name: 'apple-mobile-web-app-capable', content: 'yes' },
-            { name: 'application-name', content: 'Bio | moso.io' },
-            { name: 'msapplication-TileColor', content: '#263238' },
-            { name: 'theme-color', content: '#263238' }
-        ],
+    watch: ['~/config/*'],
 
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'manifest', href: '/manifest.json' }
-        ]
-    },
+    meta,
 
-    /*
-    ** CSS
-    */
     css: [
         '~/node_modules/flexgrid.io/dist/flexgrid.min.css',
         '~/assets/sass/main.scss'
     ],
 
-    /*
-    ** Customize the progress bar color
-    */
     loading: { color: '#3B8070' },
 
-    /*
-    ** Third-party modules
-    */
     modules: [
+        '@nuxtjs/axios',
+        ['@nuxtjs/dotenv', { systemvars: true }],
+        '@nuxtjs/pwa',
         'nuxt-webfontloader'
     ],
 
-    /*
-    ** Webfontloader
-    */
+    buildModules: [
+        '@nuxtjs/eslint-module'
+    ],
+
     webfontloader: {
         google: {
             families: ['Source+Sans+Pro:300,400,600&display=swap']
         }
     },
 
-    /*
-    ** Build configuration
-    */
-    build: {
-        /*
-        ** Run ESLint on save
-        */
-        extend (config, { isDev, isClient }) {
-            if (isDev && isClient) {
-                config.module.rules.push({
-                    enforce: 'pre',
-                    test: /\.(js|vue)$/,
-                    loader: 'eslint-loader',
-                    exclude: /(node_modules)/
-                })
-            }
-        }
-    }
-}
+    env,
 
+    manifest,
+
+    build
+}
